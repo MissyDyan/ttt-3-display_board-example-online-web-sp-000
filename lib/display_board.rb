5fba4fcd0a21c2d647f6e52642a50847ec27632f
Diane Phuong Nguyen <dianenguyen@gmail.com>
2021-06-10 02:35:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -22,9 +22,9 @@ def display_board
   # puts "-----------"
   # puts " #{board[6]} | #{board[7]} | #{board[8]}"
   # # end
-  # 
+  #
   # output = capture_puts{ display_board }
-  # 
+  #
   # expected_output  = "   |   |   \n"
   # expected_output += "-----------\n"
   # expected_output += "   |   |   \n"
