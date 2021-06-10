04961906ef6b0d4905c2a01ea4ff618f48c0fa84
Diane Phuong Nguyen <dianenguyen@gmail.com>
2021-06-10 02:47:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,7 +15,7 @@ def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  prints display_board
+  print display_board
   # expect(output).to eq(expected_output)
   # puts " #{board[0]} | #{board[1]} | #{board[2]}"
   # puts "-----------"
